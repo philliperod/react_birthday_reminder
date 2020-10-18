5af77d0aa5acd43ab0ba0fd6347c5cc3aa540301
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-18 11:05:40 -0500

    added list.js and data.js; added starting css coding from smilga's github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
+import List from './List';
+// import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h2>Birthday Reminder Project</h2>;
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Bernie Sanders',
+    age: 65,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bernie_Sanders_in_March_2020.jpg/440px-Bernie_Sanders_in_March_2020.jpg'
+  },
+  {
+    id: 2,
+    name: 'Alexandria Ocasio-Cortez',
+    age: 31,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Alexandria_Ocasio-Cortez_Official_Portrait.jpg/440px-Alexandria_Ocasio-Cortez_Official_Portrait.jpg'
+  },
+  {
+    id: 3,
+    name: 'Ilhan Omar',
+    age: 38,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Ilhan_Omar%2C_official_portrait%2C_116th_Congress_%28cropped%29.jpg/440px-Ilhan_Omar%2C_official_portrait%2C_116th_Congress_%28cropped%29.jpg'
+  },
+  {
+    id: 4,
+    name: 'Ayanna Pressley',
+    age: 46,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Ayanna_Pressley_Portrait.jpg/440px-Ayanna_Pressley_Portrait.jpg'
+  },
+  {
+    id: 5,
+    name: 'Rashida Tlaib',
+    age: 44,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Rashida_Tlaib%2C_official_portrait%2C_116th_Congress.jpg/440px-Rashida_Tlaib%2C_official_portrait%2C_116th_Congress.jpg'
+  }
+];
