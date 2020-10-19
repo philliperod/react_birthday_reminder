fffc6061e50f536b06f0785566b3529662eab7e6
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-19 11:55:08 -0500

    refactored; renamed the state value and function in the useState hook; added a <main> tag to put all the other tags within; for <h3> added an array.length to showcase the total number of birthdays; in the List component, setup a prop equaled to the state value 'people' and removed the map()method

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import data from './data';
 import List from './List';
 
 function App() {
-  const [person, setPerson] = useState(data);
+  const [people, setPeople] = useState(data);
   return (
-    <section className="container">
-      <h3>Birthday Reminder Project</h3>
-      <section className="person">
-        {person.map((listing) => {
-          return <List key={data.id} {...listing} />;
-        })}
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today</h3>
+        <section className="person">
+          <List people={people} />
+        </section>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
-      <button type="button" onClick={() => setPerson([])}>
-        Clear All
-      </button>
-    </section>
+    </main>
   );
 }
 
 export default App;
 
-// Things that I learned without help:
-// you need to import {useState} hook in order to use it; forgot about that
-// for the parameter for the hook, you use the objects/data/etc in order to access and control that info
-// using destructured object makes it easier to read and to use the props at face value as parameters or variables rather than using object dot notation
-// List component takes in a destructured parameter and returns the format of the container with props in its appropriate place
-// in the App component, this is where you will use the hook that will grab the object data from data.js and control it
-// this will also return a overall format in your page using other components to fill in the details
-// used map() method to iterate over each prop and its values in a container and returning a component that will output that info
-// the hook state function is where you will manipulate that data into something
+// Refactoring along the tutorial
+// created a <main> tag and moved other tags inside
+// created a hook [people,setPeople] with its DEFAULT data it gets from data.js
+// data.js is an array of objects which is not name but rather has 'export default'
+// just an alternate option to use if not naming the array of objects (probably not the best way so always name your arrays which can be set into a variable)
+// with the List component, this is where you will pass the array from data.js in the App function
+// so you will set up (people={people}) which is the people prop which equals to the people state value; that state value is the data array
+// next, you place in array.length to show how many individual's birthdays are in the array
