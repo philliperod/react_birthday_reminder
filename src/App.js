410a200aace84a1ae35d0e3015a1a27bcf63ffc4
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-19 09:50:38 -0500

    created a useState hook that access the data.js object props/values and use a state function to clear it from displaying in the container via button onClick

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 
 function App() {
+  const [person, setPerson] = useState(data);
   return (
     <section className="container">
       <h3>Birthday Reminder Project</h3>
       <section className="person">
-        {data.map((listing) => {
+        {person.map((listing) => {
           return <List key={data.id} {...listing} />;
         })}
       </section>
-
-      <button
-        onClick={() => {
-          console.log('it works');
-        }}
-      >
+      <button type="button" onClick={() => setPerson([])}>
         Clear All
       </button>
     </section>
@@ -24,3 +20,13 @@ function App() {
 }
 
 export default App;
+
+// Things that I learned without help:
+// you need to import {useState} hook in order to use it; forgot about that
+// for the parameter for the hook, you use the objects/data/etc in order to access and control that info
+// using destructured object makes it easier to read and to use the props at face value as parameters or variables rather than using object dot notation
+// List component takes in a destructured parameter and returns the format of the container with props in its appropriate place
+// in the App component, this is where you will use the hook that will grab the object data from data.js and control it
+// this will also return a overall format in your page using other components to fill in the details
+// used map() method to iterate over each prop and its values in a container and returning a component that will output that info
+// the hook state function is where you will manipulate that data into something
